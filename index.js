3755ec90ed4187f1d8e5d5f864be3953ff66ef97
Alexander Startup <alexander.startup@sky.uk>
2019-03-21 12:03:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-/* Your code goes here */
+// Write your solution in this file!
+
+var customerName = 'bob'
+var bestCustomer
+const leastFavoriteCustomer = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overWriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'steve'
+}
+
+function attemptTwoFavoriteCustomers() {
+    let customerName = 'meh'
+    let customerName = 'mehs'
+}
